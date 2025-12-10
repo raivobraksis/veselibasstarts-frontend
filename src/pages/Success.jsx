5cd9e0e7abdd6e5fc3a7e868490391469f5ac46a
raivobraksis <raivo.braksis@gmail.com>
2025-12-10 15:48:41 +0200

    Create Success.jsx

---
+++ src/pages/Success.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Success() {
+  return (
+    <section style={{ maxWidth: 600, margin: "40px auto", padding: 20 }}>
+      <h2>Paldies par pirkumu!</h2>
+      <p>Tavs maksājums ir apstiprināts. Drīzumā saņemsi e-pastu ar instrukcijām un dokumentiem.</p>
+    </section>
+  );
+}
