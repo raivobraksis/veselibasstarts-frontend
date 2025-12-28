866794ff8c7cf652056c97e642735714982d1183
raivobraksis <raivo.braksis@gmail.com>
2025-12-28 17:26:06 +0200

    update products

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -16,24 +16,28 @@ export default function Products() {
         title="Treniņu plāns zālei (8 nedēļas)"
         price="30"
         image="/images/trenins.jpg"
+        link="https://buy.stripe.com/9B6dRbgCtfd51fK8W63Nm03"
       />
 
       <ProductCard
         title="Treniņu plāns mājām (8 nedēļas)"
         price="30"
         image="/images/homegym3.jpg"
+        link="https://buy.stripe.com/3cIaEZ1Hzd4XcYsc8i3Nm01"
       />
 
       <ProductCard
         title="Recepšu grāmata (30 receptes)"
         price="30"
         image="/images/recipe.jpg"
+        link="https://buy.stripe.com/bJecN771T1mfe2w6NY3Nm02"
       />
 
       <ProductCard
         title="Svētku receptes (Ziemassvētki / Jaunais gads)"
         price="10"
         image="/images/ziemassvetki.jpg"
+        link="https://buy.stripe.com/7sYaEZae54yr9Mg4FQ3Nm00"
       />
     </div>
   );
