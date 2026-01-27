a30a8dab91a8ca2aab97d33ab9f7da95fcf3d4b3
raivobraksis <raivo.braksis@gmail.com>
2026-01-27 20:10:44 +0200

    v

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import Navbar from "./components/Navbar";
 import Products from "./pages/Products";
 import About from "./About";
-import Footer from "./components/Footer";
 
 export default function App() {
   return (
