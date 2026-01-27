bc7e8898dc886eef8f1d449d30ca5e52c3add560
raivobraksis <raivo.braksis@gmail.com>
2026-01-27 14:21:21 +0200

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,39 +2,12 @@ export default function Navbar() {
   return (
     <nav
       style={{
-        display: "flex",
-        justifyContent: "space-between",
-        alignItems: "center",
-        padding: "20px 40px",
-        backgroundColor: "#000000",
-        color: "#ffffff"
+        padding: "20px",
+        borderBottom: "1px solid #222",
+        textAlign: "center"
       }}
     >
-      {/* LOGO / NOSAUKUMS */}
-      <div style={{ fontWeight: "bold", fontSize: "20px" }}>
-        Veselības Starts
-      </div>
-
-      {/* NAVIGĀCIJA */}
-      <div style={{ display: "flex", gap: "20px" }}>
-        <a href="/" style={linkStyle}>
-          Sākums
-        </a>
-
-        <a href="/privacy.html" style={linkStyle}>
-          Privātuma politika
-        </a>
-
-        <a href="/terms.html" style={linkStyle}>
-          Noteikumi
-        </a>
-      </div>
+      <h1 style={{ margin: 0 }}>Veselības Starts</h1>
     </nav>
   );
 }
-
-const linkStyle = {
-  color: "#ffffff",
-  textDecoration: "none",
-  fontSize: "14px"
-};
