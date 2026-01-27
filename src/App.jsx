6817f6b41a20869f17b83bfe56309dbdea4ab4f6
raivobraksis <raivo.braksis@gmail.com>
2026-01-27 20:03:35 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,14 @@
 import Navbar from "./components/Navbar";
-import Products from "./components/Products";
+import Products from "./pages/Products";
+import About from "./About";
 import Footer from "./components/Footer";
 
 export default function App() {
   return (
-    <div
-      style={{
-        minHeight: "100vh",
-        backgroundColor: "#000000",
-        color: "#ffffff",
-        display: "flex",
-        flexDirection: "column"
-      }}
-    >
+    <div>
       <Navbar />
-
-      <main style={{ flex: 1 }}>
-        <Products />
-      </main>
-
+      <Products />
+      <About />
       <Footer />
     </div>
   );
