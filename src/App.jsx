1ad7d81daf8411d8887c11eb00e147442c91f469
raivobraksis <raivo.braksis@gmail.com>
2026-01-27 14:19:45 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,25 @@
 import Navbar from "./components/Navbar";
+import Products from "./components/Products";
 import Footer from "./components/Footer";
-import CookieBanner from "./components/CookieBanner";
-import Products from "./pages/Products";
-import Privacy from "./pages/Privacy";
-import Terms from "./pages/Terms";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <div
-        style={{
-          minHeight: "100vh",
-          backgroundColor: "#000000",
-          display: "flex",
-          flexDirection: "column"
-        }}
-      >
-        <Navbar />
+    <div
+      style={{
+        minHeight: "100vh",
+        backgroundColor: "#000000",
+        color: "#ffffff",
+        display: "flex",
+        flexDirection: "column"
+      }}
+    >
+      <Navbar />
 
-        <div style={{ flex: 1 }}>
-          <Routes>
-            <Route path="/" element={<Products />} />
-            <Route path="/privacy" element={<Privacy />} />
-            <Route path="/terms" element={<Terms />} />
-          </Routes>
-        </div>
+      <main style={{ flex: 1 }}>
+        <Products />
+      </main>
 
-        <Footer />
-        <CookieBanner />
-      </div>
-    </BrowserRouter>
+      <Footer />
+    </div>
   );
 }
