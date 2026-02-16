4adf41c38825d5695435e2f5bdea3bf9833a4a7b
raivobraksis <raivo.braksis@gmail.com>
2026-02-16 18:39:25 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import Navbar from "./components/Navbar";
 import Products from "./pages/Products";
 import About from "./pages/about";
 import Footer from "./components/Footer";
-import ProductCard from "./components/ProductCard";
 
 export default function App() {
   return (
